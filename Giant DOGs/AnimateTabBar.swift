357c4f5d4287c8359490a9757a85e72f53cc42ef
VensonLiou <zxc1234529@gmail.com>
2019-03-23 07:37:08 +0800

    initial commit

---
+++ Giant DOGs/AnimateTabBar.swift
@@ -0,0 +1,63 @@
+//
+//  AnimateTabBar.swift
+//  Giant DOGs
+//
+//  Created by 劉玟慶 on 2019/3/23.
+//  Copyright © 2019 劉玟慶. All rights reserved.
+//
+
+import UIKit
+
+class AnimateTabBar: NSObject,UITabBarControllerDelegate {
+    
+    func tabBarController(_ tabBarController: UITabBarController, animationControllerForTransitionFrom fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return ScrollingAnim(tabBarController : tabBarController)
+    }
+    
+}
+
+
+class ScrollingAnim: NSObject, UIViewControllerAnimatedTransitioning{
+    weak var transitionContext: UIViewControllerContextTransitioning?
+    var tabBarController : UITabBarController!
+    var fromIndex = 0
+    
+    init(tabBarController : UITabBarController) {
+        self.tabBarController = tabBarController
+        self.fromIndex = tabBarController.selectedIndex
+    }
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.25
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        self.transitionContext = transitionContext
+        let containerView = transitionContext.containerView
+        
+        let fromView = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)
+        
+        let toView = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)
+        containerView.addSubview(toView!.view)
+        
+        
+        var width = toView?.view.bounds.width
+
+        if tabBarController.selectedIndex < fromIndex {
+            width = -width!
+        }
+        
+        
+        toView!.view.transform = CGAffineTransform(translationX: width!, y: 0)
+        
+        UIView.animate(withDuration: self.transitionDuration(using: (self.transitionContext)), animations: {
+            toView?.view.transform = CGAffineTransform.identity
+            fromView?.view.transform = CGAffineTransform(translationX: -width!, y: 0)
+            
+        }, completion: { _ in
+            
+            fromView?.view.transform = CGAffineTransform.identity
+            self.transitionContext?.completeTransition(!(self.transitionContext?.transitionWasCancelled)!)
+        })
+    }
+}
